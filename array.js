116a1a17a342e795a3b72896b00227bfff2933dc
Soumava Das <soumavadas06@gmail.com>
2023-11-02 15:45:06 +0000

    learned array of JavaScript

---
+++ array.js
@@ -0,0 +1,14 @@
+var arr = [90, true, "Cuvette", "tech", {score: 56}, 40, 50, 33, 99]
+// console.log(typeof arr)
+// arr.push(null);
+// arr.pop()
+// arr.shift()
+// arr.shift()
+// arr.unshift(100)
+// let arrayOfNo = prompt('enter no.')
+
+console.log(arr);
+
+for(let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
