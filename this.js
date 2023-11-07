bb4351b416708f47930094c46c6c1fc0351501f6
Soumava Das <soumavadas06@gmail.com>
2023-11-07 19:21:39 +0000

    update

--- this.js
+++ this.js
@@ -1,5 +1,3 @@
-"use strict"
-
 // 1. global context
 
 //console.log(this);
@@ -27,11 +25,11 @@
 // ************arrow function********************
 
 // 1. global context
-let dummyArrow = ()=> {
-    console.log(this);
-}
+// let dummyArrow = ()=> {
+//     console.log(this);
+// }
 
-dummyArrow();
+// dummyArrow();
 
 // 2. objects and object method
 
@@ -45,5 +43,26 @@ let obj = {
 
 obj.dummy();
 
+let obj1 = {
+    property1: "sample1",
+    property2: [1,2,3],
+    dummy: function normal() {
+        ()=>{
+            console.log(this);
+        }
+    }
+}
+
+obj1.dummy();
 
 //3. constructor
+
+//4. this in strict mode
+
+"use strict"
+// function test() {
+//     console.log(this);
+// }
+
+// console.log(this);    // Called in the global scope
+// test()
