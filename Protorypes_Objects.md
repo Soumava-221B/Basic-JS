c209034b20758ff8f92cbf37c5fe42a8d04c121d
Soumava Das <soumavadas06@gmail.com>
2023-11-08 14:25:41 +0000

    prototypes

---
+++ Protorypes_Objects.md
@@ -0,0 +1,6 @@
+## Agenda: Prototypes
+1. Objects
+2. adding behaviour to objects
+3. function to create object
+4. Prototype: what and why?
+5. Properties around prototype
---
+++ script_prototype.js
@@ -0,0 +1,10 @@
+let person = {
+    name: "Alpha",
+    age: 18,
+    talk: function () {
+        console.log('talking now...')
+    }
+}
+
+person.favColor = "red";
+console.log(person);
