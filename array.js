5ca293f8c954e1d15564008a9522b14c7f113f2d
soumava <soumavadas06@gmail.com>
2023-11-12 23:59:36 +0530

    h

--- array.js
+++ array.js
@@ -4,9 +4,9 @@ var arr = [90, true, "Cuvette", "tech", {score: 56}, 40, 50, 33, 99]
 // arr.pop()
 // arr.shift()
 // arr.shift()
-// arr.unshift(100)
-// let arrayOfNo = prompt('enter no.')
-//const arr = [90, true, "Cuvette", "tech", {score: 56}, 40, 50, 33, 99]
+arr.unshift(100)
+let arrayOfNo = prompt('enter no.')
+const arr = [90, true, "Cuvette", "tech", {score: 56}, 40, 50, 33, 99]
 arr.unshift(100)
 console.log(arr);
 
